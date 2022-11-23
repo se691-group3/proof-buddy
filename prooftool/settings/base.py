b01040fb04456e5913371225b213bcebb9c7ae4e
Nicole Itchon <nci27@drexel.edu>
2022-11-22 23:58:24 -0500

    Modified static root

--- prooftool/settings/base.py
+++ prooftool/settings/base.py
@@ -123,11 +123,11 @@ LOGGING = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
     'proofchecker/static/proofchecker/'
 ]
 
